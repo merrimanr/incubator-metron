3ed45b4dfa271b5a3ebeb52a5d4d8dad05597990
iraghumitra <raghumitra.ksv@gmail.com>
2017-02-23 20:29:58 +0530

    initial commit for list view

---
+++ metron-interface/metron-alerts/src/app/alerts-list/alerts-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AlertsListComponent } from './alerts-list.component';
+
+describe('Component: AlertsList', () => {
+  it('should create an instance', () => {
+    let component = new AlertsListComponent();
+    expect(component).toBeTruthy();
+  });
+});
